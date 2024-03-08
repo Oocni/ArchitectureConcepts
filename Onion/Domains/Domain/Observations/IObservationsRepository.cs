144842a08d654ad6c9be3c3cf3a1e66f91eddd7d
Oocni <abos.pubs.ng@gmail.com>
2024-03-08 10:33:38 +0100

    Add comments on Observation entity and IObservationsRepository.

--- Onion/Domains/Domain/Observations/IObservationsRepository.cs
+++ Onion/Domains/Domain/Observations/IObservationsRepository.cs
@@ -2,10 +2,39 @@ namespace Domains.Observations;
 
 public interface IObservationsRepository
 {
+    /// <summary>
+    /// Get an observation by its id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Returns the observation or null if not find</returns>
     Task<Observation?> GetAsync(int id, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Get all observations
+    /// </summary>
+    /// <param name="id">Id to begin the search from</param>
+    /// <param name="limit">Nb of observations to return</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Returns the observations</returns>
     Task<IEnumerable<Observation>> GetAllAsync(int? id, int limit, CancellationToken cancellationToken = default);
     
+    /// <summary>
+    /// Add an observation
+    /// </summary>
+    /// <param name="observation"></param>
+    /// <returns>Returns the id of the observation</returns>
     int Add(Observation observation);
+    
+    /// <summary>
+    /// Update an observation
+    /// </summary>
+    /// <param name="observation"></param>
     void Update(Observation observation);
+    
+    /// <summary>
+    /// Delete an observation
+    /// </summary>
+    /// <param name="observation"></param>
     void Delete(Observation observation);
 }
--- Onion/Domains/Domain/Observations/Observation.cs
+++ Onion/Domains/Domain/Observations/Observation.cs
@@ -1,30 +1,76 @@
 ﻿namespace Domains.Observations;
 
+/// <summary>
+/// Observation entity
+/// </summary>
+/// <param name="name"></param>
+/// <param name="createdDate"></param>
+/// <param name="createdBy"></param>
+/// <param name="description">Can be null</param>
 public class Observation(
     string name,
     DateTime createdDate,
     string createdBy,
     string? description = null)
 {
+    /// <summary>
+    /// Id of the observation
+    /// </summary>
     public int? Id { get; init; }
+    
+    /// <summary>
+    /// Created date of the observation
+    /// </summary>
     public DateTime CreatedDate { get; init; } = createdDate;
+    
+    /// <summary>
+    /// Created by of the observation
+    /// </summary>
     public string CreatedBy { get; init; } = createdBy;
 
+    /// <summary>
+    /// Name of the observation
+    /// </summary>
     public string Name { get; private set; } = name;
+    
+    /// <summary>
+    /// Description of the observation
+    /// </summary>
     public string? Description { get; private set; } = description;
+    
+    /// <summary>
+    /// Deleted date of the observation
+    /// </summary>
     public DateTime? DeletedDate { get; private set; }
+    
+    /// <summary>
+    /// Deleted by of the observation
+    /// </summary>
     public string? DeletedBy { get; private set; }
     
+    /// <summary>
+    /// Update the name of the observation
+    /// </summary>
+    /// <param name="name"></param>
     public void UpdateName(string name)
     {
         Name = name;
     }
     
+    /// <summary>
+    /// Update the description of the observation
+    /// </summary>
+    /// <param name="description"></param>
     public void UpdateDescription(string description)
     {
         Description = description;
     }
     
+    /// <summary>
+    /// Set the deleted date and deleted by of the observation
+    /// </summary>
+    /// <param name="deletedDate"></param>
+    /// <param name="deletedBy"></param>
     public void Delete(DateTime deletedDate, string deletedBy)
     {
         DeletedDate = deletedDate;
